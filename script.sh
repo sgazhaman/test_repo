e0c81bddb2f6af06e7b86daed027862a147156af
sgazhaman <sgazhaman@gmail.com>
2015-01-01 23:58:54 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
-current_date=$(date +%T)
-touch /root/test/file_$current_date._from_script
+#!/bin/bash
+# script to send simple email 
+# email subject
+SUBJECT="SET-EMAIL-SUBJECT"
+# Email
+FROM="support@ontrq.com"
+TO="s.gazhaman@ontrq.com"
+CC="s.gazhaman@ontrq.com"
+BCC="s.gazhaman@ontrq.com"
+# Email text/message
+EMAILMESSAGE="/tmp/emailmessage.txt"
+echo "This is an email message test"> $EMAILMESSAGE
+# send an email using /bin/mail
+/bin/mail -s "$SUBJECT" -b "$BCC" -c "$CC"  -r "$FROM" "$TO" < $EMAILMESSAGE
